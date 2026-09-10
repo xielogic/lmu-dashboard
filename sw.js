63d156b47b5a768ff8caeffdc84f87cdc36a4c05
xielogic <xielogic@outlook.com>
2026-09-10 17:06:40 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='lmu-dashboard-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
